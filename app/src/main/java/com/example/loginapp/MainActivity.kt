2f384fff0857bb13ef54ca0248d60d014da3b930
shubhamj25 <sjain251298@gmail.com>
2021-02-18 21:21:10 +0530

    Bug Fixes in LocationActivity.kt

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.loginapp
 import android.content.Intent
 import android.os.Bundle
 import com.example.loginapp.screens.prelogin.activity.PreLoginViewPagerActivity
-
 open class MainActivity : BaseActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
